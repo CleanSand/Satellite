f669dc2542ebc3682fb9553d087ffca308aa6113
KniazNaverno <kagami228k@mail.ru>
2022-12-15 16:21:10 +0700

    15.12.2022

--- Satellite/ViewModels/HomePageVM.cs
+++ Satellite/ViewModels/HomePageVM.cs
@@ -25,6 +25,8 @@ public class HomePageVM : ViewModelBase
     private static ObservableCollection<Solar> _favouritessolars = new ObservableCollection<Solar>();
     public static ObservableCollection<Solar> Favouritessolars => _favouritessolars;
     private static Solar _objectSolar;
+    private string _searchstring;
+    public static Solar _serchingSolar;
     public static async Task Main()
     {
         using var client = new HttpClient();
@@ -65,7 +67,33 @@ public class HomePageVM : ViewModelBase
             informationWindow.ShowDialog(_homePageWindow);
         }
     }
-
+    public string Searchstring
+    {
+        get => _searchstring;
+        set => this.RaiseAndSetIfChanged(ref _searchstring, value);
+    }
+    public ReactiveCommand<Window, Unit> SubmitSearch { get; }
+    private void SubmitSearchImpl(Window obj)
+    {
+        _serchingSolar = Solars.FirstOrDefault(x => x.englishName == _searchstring);
+        if (_serchingSolar != null)
+        {
+            _solars.Clear();
+            _solars.Add(new Solar
+            {
+                bodyType = _serchingSolar.bodyType,
+                englishName = _serchingSolar.englishName,
+                id = _serchingSolar.id,
+                isPlanet = _serchingSolar.isPlanet,
+                perihelion = _serchingSolar.perihelion
+            });
+        }
+        else
+        {
+            _solars.Clear();
+            Main();
+        }
+    }
     private HomePageWindow _homePageWindow;
     public ReactiveCommand<Window, Unit> OpenWindow { get; }
 
@@ -104,5 +132,6 @@ public class HomePageVM : ViewModelBase
         _homePageWindow = window;
         OpenWindow = ReactiveCommand.Create<Window>(OpenWindowImpl);
         OpenWindow2 = ReactiveCommand.Create<Window>(OpenWindow2Impl);
+        SubmitSearch = ReactiveCommand.Create<Window>(SubmitSearchImpl);
     }
 }
